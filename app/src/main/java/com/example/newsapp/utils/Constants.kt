165048b6b592a203577592e02b47137a51f3035b
Ekaterina Rykova <katrykova2002@yandex.ru>
2023-01-28 10:34:19 +0300

    lesson-3

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "1575e3b545f54be583cd5624df5a6394"
+    }
+}
